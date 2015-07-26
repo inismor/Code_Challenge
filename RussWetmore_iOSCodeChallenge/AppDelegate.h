029de3178def883b5da7b0dc345624cef08ce93c
Russ Wetmore <russ.wetmore@reachhealth.com>
2015-07-26 18:51:44 -0400

    Initial Commit

---
+++ RussWetmore_iOSCodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RussWetmore_iOSCodeChallenge
+//
+//  Created by Russ Wetmore on 7/26/15.
+//  Copyright (c) 2015 the easydesign company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RussWetmore_iOSCodeChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RussWetmore_iOSCodeChallenge
+//
+//  Created by Russ Wetmore on 7/26/15.
+//  Copyright (c) 2015 the easydesign company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
